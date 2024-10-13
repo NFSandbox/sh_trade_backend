07c094187764c951b598420f05d5133849a78874
Oyasuminasai <61616918+nfnfgo@users.noreply.github.com>
2024-10-13 22:21:42 +0800

    workon: by_name search

--- provider/search/basic.py
+++ provider/search/basic.py
@@ -1,5 +1,7 @@
 import asyncio
 
+import asyncstdlib as alib
+
 from typing import (
     Annotated,
     cast,
@@ -15,16 +17,11 @@ from typing import (
 )
 
 from dataclasses import dataclass
-from functools import cached_property
+from functools import cached_property, cache
 
 from pydantic import BaseModel
-
-from inspect import isawaitable
-
-from asyncer import asyncify
-from asyncio import iscoroutine
-
 from loguru import logger
+
 from sqlalchemy import select, func, Column, distinct, union_all, union
 from sqlalchemy.orm import selectinload, QueryableAttribute, aliased
 from sqlalchemy.sql import and_, or_
@@ -58,14 +55,72 @@ class Searcher:
     def __init__(self, keyword: str) -> None:
         self.keyword = keyword
 
-    @cached_property
-    def by_name_keyword(self):
+    def clear_cache(self):
+        """
+        Clear caches of this Searcher instance.
+        """
+        # keyword properties
+        del self.by_name_keyword
+        del self.by_tag_keywords
+
+    @alib.cached_property
+    async def by_name_keyword(self):
+        return self._get_by_name_keyword()
+
+    @alib.cached_property
+    async def by_tag_keywords(self):
+        return await self._get_by_tag_keywords(remove_invalid=True)
+
+    def _get_by_name_keyword(self):
         """Keyword used in by-name search"""
         return self.keyword.strip()
 
-    @cached_property
-    async def by_tag_keywords(self):
-        """Set of tags used in by-tag search"""
+    async def _get_by_tag_keywords(self, remove_invalid: bool = True):
+        """
+        Set of tags used in by-tag search
+
+        Args
+
+        - `remove_invalid` Remove the tag in the set which is not a valid tag in database.
+        """
+        # split keyword string into tag candidates
+        splited_tag_str = set([t.strip() for t in self.keyword.split(" ")])
+
+        # check database to filter non-exists tags if needed
+        if remove_invalid:
+            logger.debug("remove_invalid enabled")
+            async with session_manager() as ss:
+                # get valid tags
+                stmt = select(orm.Tag.name).where(orm.Tag.name.in_(splited_tag_str))
+                valid_tag_set = set((await ss.scalars(stmt)).all())
+                logger.debug(f"valid tags: {valid_tag_set}")
+
+                # intersection update
+                splited_tag_str.intersection_update(valid_tag_set)
+
+        return splited_tag_str
+
+    async def _get_by_name_search_result(self, ignore_processing_items: bool = True):
+        """
+        Search items in database by name.
+
+        Args
+
+        - `ignore_processing_item` Ignore the items which has a processing transaction
+        """
+        keyword = await self.by_name_keyword
+        logger.debug(f"Using by_name keyword: {keyword}")
+
+        stmt = (
+            select(orm.Item)
+            .where(orm.Item.name.like(f"%{keyword}%"))
+            .where(orm.Item.state == orm.ItemState.valid)
+        )
+
+        if ignore_processing_items:
+            stmt = stmt.where(orm.Item.processing_trade == None)
+
+        logger.debug(f"by_name search stmt: \n {stmt}")
+
         async with session_manager() as ss:
-            pass
-        return set([t.strip() for t in self.keyword.split(" ")])
+            return (await ss.scalars(stmt)).all()
