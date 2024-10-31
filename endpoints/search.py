4ab426abae0b32747c1f3eba2c21d61d9fba6f69
Oyasuminasai <61616918+nfnfgo@users.noreply.github.com>
2024-10-31 18:48:55 +0800

    feat: Add search endpoints

--- endpoints/search.py
+++ endpoints/search.py
@@ -11,14 +11,10 @@ from schemes import db as db_sche
 from schemes import general as gene_sche
 from schemes import sql as orm
 
-from provider import user as user_provider
-from provider import database as db_provider
-from provider.database import try_commit
-from provider import item as item_provider
-from provider import fav as fav_provider
 from provider.user import CurrentUserDep, CurrentUserOrNoneDep
 from provider import auth as auth_provider
 from provider.auth import PermissionsChecker
+from provider.search import Searcher
 
 from provider.database import SessionDep
 from exception import error as exc
@@ -27,18 +23,29 @@ from exception import error as exc
 search_router = APIRouter()
 
 
-@search_router.get("/item")
-async def search_items(
+@search_router.post("/item/by_name")
+async def search_items_by_name(
+    q: Annotated[bool, Depends(PermissionsChecker({"search:item:by_name"}))],
+    ss: SessionDep,
     keyword: str = Query(min_length=1),
-    by_name: bool = True,
-    by_tag: bool = True,
+    pagination: gene_sche.PaginationConfig | None = None,
 ):
     """
-    Search items
+    Search items by name
     """
-    # keyword used in by-name search
-    name_search_keyword = keyword
-    # tags used in by_tag search
-    tag_search_keywords = [t.strip() for t in keyword.split(" ")]
+    searcher = Searcher(keyword, pagination=pagination)
+    return await searcher.by_name_search(ss)
 
-    # TODO
+
+@search_router.post("/item/by_tags")
+async def search_items_by_tags(
+    q: Annotated[bool, Depends(PermissionsChecker({"search:item:by_tags"}))],
+    ss: SessionDep,
+    keyword: str = Query(min_length=1),
+    pagination: gene_sche.PaginationConfig | None = None,
+):
+    """
+    Search items by name
+    """
+    searcher = Searcher(keyword, pagination=pagination)
+    return await searcher.by_tags_search(ss)
